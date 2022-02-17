3092e946061bdf3d185092b056381d613615be6f
asdacosta <dacostasilvanus@gmail.com>
2022-02-17 23:36:29 +0000

    Updated file to create binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to parent
+ * @value: value of node
+ *
+ * Return: pointer to new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = calloc(1, sizeof(binary_tree_t));
+
+	
+	if (new == NULL)
+		return (NULL);
+
+	new->parent = parent;
+	new->n = value;
+	return (new);
+}
