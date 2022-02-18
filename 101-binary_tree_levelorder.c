94a5e1d070977be138ec1d923b403172912fc239
asdacosta <dacostasilvanus@gmail.com>
2022-02-18 02:45:16 +0000

    Modified documentation

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- *  * binary_tree_depth - measures the depth of a node in a binary tree
- *   * @tree: is a pointer to the node to measure the depth
- *    * Return: The depth of the node, if tree is NULL return 0
- *     */
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: is a pointer to the node to measure the depth
+ * Return: The depth of the node, if tree is NULL return 0
+ */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
